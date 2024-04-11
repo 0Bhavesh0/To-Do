4f5bdfa1e9d1e9a2a2fc7cb9e67c5719aab02f7d
BhaveshS <bhaveshsutar001@gmail.com>
2024-04-11 12:45:55 +0530

    Refactor code

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-const inputBox = document.querySelector('#input-box')
-const listContainer = document.querySelector('.list-container')
-const btn = document.querySelector('button')
-const completed = document.querySelector('.completed')
+// Declare constants for DOM elements
+const inputBox = document.querySelector('#input-box');
+const listContainer = document.querySelector('.list-container');
+const btn = document.querySelector('button');
+const completed = document.querySelector('.completed');
+const completedPara = document.querySelector('.completed-para');
 
 
 function addTask() {
-    if (inputBox.value == '') alert('Add your task')
-    else {
-        let li = document.createElement('li')
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement('span')
-        span.innerHTML = '\u00d7'
-        li.appendChild(span)
+
+    if (inputBox.value === '') {
+        alert('Please enter a task before adding.');
+        return;
     }
+
+
+    const taskItem = document.createElement('li');
+    taskItem.textContent = inputBox.value;
+    const closeButton = document.createElement('span');
+    closeButton.textContent = '\u00d7';
+    taskItem.appendChild(closeButton);
+    listContainer.appendChild(taskItem);
+
+    completedPara.classList.add('active');
+
     inputBox.value = '';
-    inputBox.focus()
+    inputBox.focus();
 }
 
 
 inputBox.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
-        addTask()
-    }
-})
-
-
-btn.addEventListener('click', addTask)
-
-listContainer.addEventListener('click',function(e){
-    if(e.target.tagName == 'LI'){
-        e.target.classList.toggle('checked')
-        completed.appendChild(e.target)
-
-    }
-    else if(e.target.tagName == 'SPAN'){
-        e.target.parentElement.remove()
-    }
-    
-})
-
-completed.addEventListener('click', function(e) {
-    if (e.target.tagName === 'SPAN') {
-      e.target.parentElement.remove();
+    if (e.key === 'Enter') {
+        addTask();
     }
 });
+
+btn.addEventListener('click', addTask);
+
+
+listContainer.addEventListener('click', (e) => {
+    const target = e.target
+    if (target.tagName === 'LI') {
+        target.classList.toggle('checked');
+        completed.appendChild(target);
+    }
+
+    else if (target.tagName === 'SPAN') {
+        target.parentElement.remove();
+    }
+});
+
+
+completed.addEventListener('click', (e) => {
+
+    if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+
+        if (completed.querySelectorAll('li').length === 0) {
+            completedPara.classList.remove('active');
+        }
+    }
+});
+
